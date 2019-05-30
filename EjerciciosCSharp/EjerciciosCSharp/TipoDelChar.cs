43e995f7233a33d0191a5e379990b1fef28e8a7b
Jose Fontenla Gonzalez <jfontenl@everis.com>
2019-05-30 15:52:49 +0200

    tipo del dato

---
+++ EjerciciosCSharp/EjerciciosCSharp/TipoDelChar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjerciciosCSharp
+{
+    public class TipoDelChar
+    {
+        public static void RealType()
+        {
+            var result = "";
+            Console.WriteLine("Escriba un caracter : ");
+            var linea = Console.ReadLine();
+
+            if (linea.Length > 1)
+            {
+                Console.WriteLine("Ha escrito mas de un caracter");
+            }
+            else
+            {
+                var character = char.Parse(Console.ReadLine());
+                int number;
+                var isNumber = int.TryParse(character.ToString(), out number);
+                if (isNumber)
+                {
+                    Console.WriteLine("Es un numero");
+                }
+                else if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+                {
+                    Console.WriteLine("Es una vocal");
+                }
+                else
+                {
+                    Console.WriteLine("Es una consonante");
+                }
+                Console.ReadLine(); 
+            }
+        }
+    }
+}

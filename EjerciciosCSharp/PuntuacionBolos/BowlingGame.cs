0cb63437d565faa79aaf0a572a26d58d45ce96cb
Jose Fontenla Gonzalez <jfontenl@everis.com>
2019-06-14 12:56:12 +0200

    Semiplenos

--- EjerciciosCSharp/PuntuacionBolos/BowlingGame.cs
+++ EjerciciosCSharp/PuntuacionBolos/BowlingGame.cs
@@ -24,13 +24,57 @@ namespace PuntuacionBolos
             
         }
 
-        public int Socre() {
+        public int Score() {
             int result = 0;
-            foreach(int frame in _frames)
+            int count = 0;
+            foreach(string frame in _frames)
             {
-                result += frame;
+                var points = new int();
+                var isInteger = int.TryParse(frame, out points);
+               
+                if (isInteger)
+                {
+                    result += points;
+                }else if (isSemiFull(frame) || isFull(frame))
+                {
+                    if (isSemiFull(frame))
+                    {
+                        result += ScoreSemiFull(_frames, count);
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+                count++;
+                
             }
             return result;
         }
+
+        public bool isSemiFull(string frame)
+        {
+            return frame.Trim() == "/";
+        }
+        public bool isFull(string frame)
+        {
+            return frame.Trim().ToUpper() == "X";
+        }
+        public int ScoreSemiFull(ArrayList _frames,int position )
+        {
+            var Score = new int();
+            var pointsNextPosition = _frames[position + 1].ToString();
+            Score = 10 + int.Parse(pointsNextPosition) - int.Parse(_frames[position-1].ToString());
+            return Score;
+        }
+
+        public int ScoreFull(ArrayList _frames, int position)
+        {
+            var score = new int();
+            score = 10 + int.Parse(_frames[position + 1].ToString()) +int.Parse( _frames[position + 2].ToString());
+            return score;
+        }
+
+
     }
 }
--- EjerciciosCSharp/xUnitPuntuacionBolos/PuntuacionesBolosTest.cs
+++ EjerciciosCSharp/xUnitPuntuacionBolos/PuntuacionesBolosTest.cs
@@ -7,29 +7,76 @@ namespace xUnitPuntuacionBolos
     public class PuntuacionesBolosTest
     {
         [Fact]
-        public void SocreWithoutPoints()
+        public void ScoreWithoutPoints()
         {
-            var framesWithoutStrikes = new ArrayList { 0,0 ,0,0 ,0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0 };
+            var framesWithoutStrikes = new ArrayList { "0","0" ,"0","0" ,"0","0", "0","0", "0","0", "0","0","0","0", "0","0", "0","0" };
             var BowlingGame = new BowlingGame(framesWithoutStrikes);
 
-            var result = BowlingGame.Socre();
+            var result = BowlingGame.Score();
             Assert.Equal(result, 0);
         }
 
         [Fact]
-        public void SocreWithPoints()
+        public void ScoreWithPoints()
         {
-            var framesWithoutStrikes1 = new ArrayList { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 ,1,0 };
+            var framesWithoutStrikes1 = new ArrayList { "1", "0", "1", "0", "1", "0", "1", "0", "1", "0", "1", "0", "1", "0", "1", "0", "1", "0","1","0" };
             var BowlingGame1 = new BowlingGame(framesWithoutStrikes1);
-            var result1 = BowlingGame1.Socre();
+            var result1 = BowlingGame1.Score();
 
-            var framesWithoutStrikes2 = new ArrayList { 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0 };
+            var framesWithoutStrikes2 = new ArrayList { "2", "0", "2", "0", "2", "0", "2", "0", "2", "0", "2", "0", "2", "0", "2", "0", "2", "0","2","0" };
             var BowlingGame2 = new BowlingGame(framesWithoutStrikes2);
-            var result2 = BowlingGame2.Socre();
+            var result2 = BowlingGame2.Score();
 
 
             Assert.Equal(result1, 10);
             Assert.Equal(result2, 20);
+        }
+
+
+
+        [Fact]
+        public void IsSemiFull()
+        {
+            var frame1 = "/";
+            var BowlingGame1 = new BowlingGame();
+            var result1 = BowlingGame1.isSemiFull(frame1);
+
+            var frame2 = "X";
+            var BowlingGame2 = new BowlingGame();
+            var result2 = BowlingGame2.isSemiFull(frame2);
+
+
+            Assert.Equal(result1, true);
+            Assert.Equal(result2, false);
+        }
+
+
+        [Fact]
+        public void FramesWithSemiPulls()
+        {
+            var framesWithoutStrikes1 = new ArrayList { "5", "/", "2", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" };
+            var BowlingGame1 = new BowlingGame(framesWithoutStrikes1);
+            var result1 = BowlingGame1.Score();
+
+            var framesWithoutStrikes2 = new ArrayList { "5", "/", "2", "/", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" }; 
+            var BowlingGame2 = new BowlingGame(framesWithoutStrikes2);
+            var result2 = BowlingGame2.Score();
+
+
+             framesWithoutStrikes2 = new ArrayList { "5", "/", "0", "/", "0", "/", "0", "/", "0", "/", "0", "/", "0", "/", "0", "/", "0", "/", "0", "/","0" };
+            BowlingGame2 = new BowlingGame(framesWithoutStrikes2);
+            var result3 = BowlingGame2.Score();
+
+            Assert.Equal(result1, 14);
+            Assert.Equal(result2, 22);
+            Assert.Equal(result3, 100);
+        }
+
+        public void FramesWithPulls()
+        {
+            var framesWithoutStrikes1 = new ArrayList { "5", "X", "2", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" };
+            var BowlingGame1 = new BowlingGame(framesWithoutStrikes1);
+            var result1 = BowlingGame1.Score();
         }
     }
 }
